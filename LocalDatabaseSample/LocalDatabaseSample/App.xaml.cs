566bfb99c7b964328d91745d3fcc534fbfcf76ed
Adolfo Ramos <ingadolfo@icloud.com>
2022-04-18 23:49:14 -0500

    Creando la instancia de la base de datos una sola vez que sea utilizable en todos lados.

--- LocalDatabaseSample/LocalDatabaseSample/App.xaml.cs
+++ LocalDatabaseSample/LocalDatabaseSample/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocalDatabaseSample.Data;
+using LocalDatabaseSample.Services;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,6 +8,21 @@ namespace LocalDatabaseSample
 {
     public partial class App : Application
     {
+
+        private static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new Database(DependencyService.Get<IFileHelper>().GetLocalFilePath("StallDB"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
