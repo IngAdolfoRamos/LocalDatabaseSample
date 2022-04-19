b8a710fb50a5259b320815c866f5fa0f96e14b8d
Adolfo Ramos <ingadolfo@icloud.com>
2022-04-18 23:56:47 -0500

    Definiendo la estructura de la tabla Stall dentro de la clase Stall

--- LocalDatabaseSample/LocalDatabaseSample/Models/Stall.cs
+++ LocalDatabaseSample/LocalDatabaseSample/Models/Stall.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,15 @@ namespace LocalDatabaseSample.Models
 {
     public class Stall
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public int phone { get; set; }
     }
 }
