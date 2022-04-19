3079534ef0300bb846577abb268c8f02b1e5afdd
Adolfo Ramos <ingadolfo@icloud.com>
2022-04-18 23:23:54 -0500

    Definiendo IFiñeHelper para la cadena de conexion y el carchivo Database para crear una nueva tabla de acuerdo al modelo Stall

---
+++ LocalDatabaseSample/LocalDatabaseSample/Data/Database.cs
@@ -0,0 +1,19 @@
+﻿using LocalDatabaseSample.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalDatabaseSample.Data
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection database;
+
+        public Database(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Stall>().Wait();
+        }
+    }
+}
---
+++ LocalDatabaseSample/LocalDatabaseSample/Models/Stall.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalDatabaseSample.Models
+{
+    public class Stall
+    {
+    }
+}
